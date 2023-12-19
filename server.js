b0cc06627b98be7e01f9fee19568f04e2870281e
SuperStar0718 <georgejohnson11305@gmail.com>
2023-12-19 10:24:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, './client/build')));
 app.use(bodyParser.json());
